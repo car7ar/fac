a743fb54a3ceae64a74327c2cb957abf6fdb439e
car7ar <car7ar@gmail.com>
2026-05-11 03:18:18 +0300

    Delete image-generator directory

--- image-generator/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-npm install
-# This installs the necessary dependencies for Puppeteer to run on Render
-# Puppeteer usually needs some system-level libraries
-# Render's environment might need these if not using a Dockerfile
